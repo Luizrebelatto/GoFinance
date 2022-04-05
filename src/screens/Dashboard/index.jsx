73f468badd151ba16a01e930ee8d5dd250994388
luiz <lbrebelatto@gmail.com>
2022-04-04 23:15:24 -0300

    fix: criação e estilização da tela dashboard

---
+++ src/screens/Dashboard/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Text } from './styles';
+
+export function DashBoard(){
+  return(
+    <Container>
+      <Text>Dashboard</Text>
+    </Container>
+  )
+}
