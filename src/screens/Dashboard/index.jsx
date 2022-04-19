9a673c09a972af02b581117689d2c5e1d948f83c
luiz <lbrebelatto@gmail.com>
2022-04-19 10:02:48 -0300

    feat: header usuário
    criação do componente que contém foto e nome do usuário

--- src/screens/Dashboard/index.jsx
+++ src/screens/Dashboard/index.jsx
@@ -2,13 +2,28 @@ import React from 'react';
 
 import { 
   Container,
-  Header
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
 } from './styles';
 
 export function DashBoard(){
   return(
     <Container>
       <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/62765965?v=4' }}/>
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Luiz</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
       </Header>
     </Container>
   )
