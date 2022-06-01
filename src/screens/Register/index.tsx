2222a35b7b4c09552ae7afb5d5b6bdc59516f2c1
luiz <lbrebelatto@gmail.com>
2022-06-01 07:43:36 -0300

    implement: salvar mais de um registro no asyncstorage sem sobrescrever

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -58,15 +58,23 @@ export function Register(){
       if(category.key === 'category')
       return Alert.alert('Selecione o tipo de categoria')
 
-    const data = {
+    const newTransaction = {
       name: form.name,
       amount: form.amount,
       transactionType,
       category: category.key
     }
-    console.log("dados: " + data);
+  
     try {
-      await AsyncStorage.setItem(dataKey, JSON.stringify(data));
+      const data = await AsyncStorage.getItem(dataKey);
+      const currentData = data ? JSON.parse(data) : [];
+
+      const dataFormatted = [
+        ...currentData,
+        newTransaction
+      ]
+
+      await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted));
     } catch (error) {
       console.log(error);
       Alert.alert("Não foi Possivel Salvar");
@@ -76,7 +84,7 @@ export function Register(){
   useEffect(() =>{
     async function loadData(){
       const data = await AsyncStorage.getItem(dataKey);
-      console.log(data);
+      console.log(JSON.parse(data!));
     }
 
     loadData();
